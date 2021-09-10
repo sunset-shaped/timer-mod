8987aeba7e089a1878cfb943f7c584a544baae16
ty <fqdingsky@gmail.com>
2021-09-10 08:57:08 +0800

    updated for 3 millisecond digits

--- timer.gd
+++ timer.gd
@@ -10,20 +10,6 @@ func _ready():
 func _process(delta):
 	#every frame, we update timer and send it to base
 	#the set_hud_text function should be used if your mod only has one value to update. this makes it compatible with the other mods.
-	base.set_hud_text("timer", make_time(base.timer))
+	base.set_hud_text("timer", base.menu.make_time(base.timer))
 	#use the make_time function to turn the time into a string
 	
-
-func make_time(timetaken):
-	var taken = stepify(timetaken, 0.01)
-	
-	var hr = floor(taken/3600)
-	var mn = floor(taken/60) - (hr * 60)
-	var sc = floor(taken) - (mn*60) - (hr*3600)
-	var mls = fmod(taken,1) * 100
-	
-	if hr == 0:
-		return "%02d:%02d.%02d" % [mn, sc, mls]
-	else:
-		return "%d:%02d:%02d.%02d" % [hr, mn, sc, mls]
-
